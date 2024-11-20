c4a99daa3b5c3301168f7e6ec6f973bf4c033c1e
giovanni-arduini <gionnibigudd@gmail.com>
2024-11-20 15:46:26 +0100

    ADD CRUD functions to controller

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,13 +1,38 @@
 // Functions
 
 //index
+function index(req, res) {
+  console.log("Ecco la lista dei post");
+  res.json(posts);
+}
 
 //show
+function show(req, res) {
+  const slug = req.params.slug;
+  console.log(`Ecco il post con slug ${slug}`);
+  const post = posts.find((p) => p.slug === slug);
+  res.json(post);
+}
 
 //store
+function store(req, res) {
+  res.send("Crea un nuovo post!");
+}
 
 //modify
+function modify(req, res) {
+  const slug = req.params.slug;
+  res.send(`Aggiorna il post con slug ${slug}`);
+}
 
 //update
+function update(req, res) {
+  const slug = req.params.slug;
+  res.send(`Modifica il post con slug ${slug}`);
+}
 
 //destroy
+function destroy(req, res) {
+  const slug = req.params.slug;
+  res.send(`Elimina il post con slug ${slug}`);
+}
