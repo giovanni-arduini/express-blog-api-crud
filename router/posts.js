f28fd13f2e0aab092ade216c296dfb34bcd69c83
giovanni-arduini <gionnibigudd@gmail.com>
2024-11-20 15:56:29 +0100

    ADD routes in router posts.js

--- router/posts.js
+++ router/posts.js
@@ -1,3 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const posts = require("../data/posts.js");
+const postController = require("../controllers/postController.js");
+
+//index
+router.get("/", postController.index);
+
+//show
+router.get("/:slug", postController.show);
+
+//store
+router.post("/", postController.store);
+
+//update
+router.put("/:slug", postController.update);
+
+//modify
+router.patch("/:slug", postController.modify);
+
+//destroy
+router.delete("/:slug", postController.destroy);
+
+module.exports = router;
